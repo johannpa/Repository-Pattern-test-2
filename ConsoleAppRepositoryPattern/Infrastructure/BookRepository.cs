5fcc295443bd084ebecd18d310aed412529f71d6
johannpa <johann_paque@hotmail.fr>
2023-10-20 00:44:11 +0200

    Create BookRepository in Infrastructure

---
+++ ConsoleAppRepositoryPattern/Infrastructure/BookRepository.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleAppRepositoryPattern.Entities;
+using ConsoleAppRepositoryPattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppRepositoryPattern.Infrastructure
+{
+    public class BookRepository : IRepository<Book>
+    {
+        public void Add(Book entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Book entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Book entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

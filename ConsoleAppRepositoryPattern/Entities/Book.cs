5ca18762e8bc4d0bfa63070b95a8f2dfae130a71
johannpa <johann_paque@hotmail.fr>
2023-10-20 00:40:20 +0200

    Create an interface and a entity

---
+++ ConsoleAppRepositoryPattern/Entities/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppRepositoryPattern.Entities
+{
+    public class Book
+    {
+        public Guid Id { get; }
+
+        public string Name { get; set; }
+        public Book(string name)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+        }
+    }
+}
---
+++ ConsoleAppRepositoryPattern/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppRepositoryPattern.Interfaces
+{
+    internal interface IRepository<T>
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        T Get(Guid id);
+    }
+}
